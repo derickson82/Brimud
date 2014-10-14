51e43efd7ac60e252cf65d0e5052470bd05ce81f
Dan Erickson <derickson82@gmail.com>
2014-10-13 21:22:47 -0600

    Remove database scripts, old configs, and add the gradle wrapper

--- src/main/scripts/sql/updatedb.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS pcharacter (
-  name VARCHAR(30) NOT NULL,
-  CONSTRAINT pk_pcharacter PRIMARY KEY (name)
-);
-
-ALTER TABLE player ADD CONSTRAINT fk_player_pcharacter FOREIGN KEY (pcharacter) REFERENCES pcharacter(name);
